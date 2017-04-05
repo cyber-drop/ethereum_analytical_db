f03fe9b2466c2a2ffa65b5cf6d88e716789b1756
Vincent Kobel <v@klaat.com>
2017-04-05 16:47:25 +0200

    Better args support + cleanup

--- ethdrain.py
+++ ethdrain.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import sys
 import asyncio
 import json
 import datetime
@@ -15,18 +14,18 @@ logging.basicConfig(filename='error_blocks.log', level=logging.ERROR)
 
 TX_INDEX_NAME = "ethereum-transaction"
 B_INDEX_NAME = "ethereum-block"
-HTTP_HEADERS = {"content-type": "application/json"}
+WEI_ETH_FACTOR = 1000000000000000000.0
 
 # Elasticsearch maximum number of connections
-ES_MAXSIZE = 10
+ES_MAXSIZE = 25
 # Elasticsearch default url
 ES_URL = "http://localhost:9200"
 # Ethereum RPC endpoint
-ETH_ENDPOINT = "http://localhost:8545"
+ETH_URL = "http://localhost:8545"
 # Parallel processing semaphore size
 SEM_SIZE = 256
 # Size of chunk size in blocks
-CHUNK_SIZE = 500
+CHUNK_SIZE = 250
 # Size of multiprocessing Pool processing the chunks
 POOL_SIZE = mp.cpu_count() + 2
 
@@ -47,7 +46,7 @@ def make_request(block):
 
 async def fetch(url, session, block, process_fn, actions):
     try:
-        async with session.post(url, data=make_request(block), headers=HTTP_HEADERS) as response:
+        async with session.post(url, data=make_request(block), headers={"content-type": "application/json"}) as response:
             process_fn(await response.json(), actions)
     except (aiohttp.ClientError, asyncio.TimeoutError) as exception:
         logging.error("block: " + str(block))
@@ -68,7 +67,7 @@ async def run(block_range, process_fn, actions):
     async with aiohttp.ClientSession() as session:
         for i in block_range:
             # pass Semaphore and session to every POST request
-            task = asyncio.ensure_future(sema_fetch(sem, ETH_ENDPOINT, session, i, process_fn, actions))
+            task = asyncio.ensure_future(sema_fetch(sem, ETH_URL, session, i, process_fn, actions))
             tasks.append(task)
 
         await asyncio.gather(*tasks)
@@ -88,7 +87,7 @@ def process_block(block, actions):
         tx["blockNumber"] = int(tx["blockNumber"], 0)
         tx["blockTimestamp"] = block_timestamp
         # Convert wei into ether
-        tx["value"] = int(tx["value"], 0) / 1000000000000000000.0
+        tx["value"] = int(tx["value"], 0) / WEI_ETH_FACTOR
         tx_value_sum += tx["value"]
         actions.append(
             {"_index": TX_INDEX_NAME, "_type": "tx", "_id": tx["hash"], "_source": tx}
@@ -133,42 +132,26 @@ def setup_process(block_range):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('-s', '--start',
-                        default='0',
-                        help='What block to start indexing'
-                        )
-
-    parser.add_argument('-e', '--end',
-                        default='1000',
-                        help='What block to finish indexing'
-                        )
-
-    parser.add_argument('-f', '--file',
-                        default=None,
-                        help="Use an input file"
-                        )
-    parser.add_argument('-u', '--esurl',
-                        default='http://localhost:9200',
-                        help='The elasticsearch url and port. Accepts all the same parameters needed as a normal Elasticsearch client expects.'
-                        )
-
-    parser.add_argument('-m', '--esmaxsize',
-                        default='10',
-                        help='The elasticsearch max chunk size'
-                        )
-
+    parser.add_argument('start_block', type=int, help='What block to start indexing.')
+    parser.add_argument('end_block', type=int, help='What block to finish indexing.')
+    parser.add_argument('-f', '--file', default=None,
+                        help='Use an input file, each block number on a new line.')
+    parser.add_argument('-u', '--esurl', default=ES_URL,
+                        help='The elasticsearch url and port. Accepts all the same parameters needed as a normal Elasticsearch client expects.')
+    parser.add_argument('-m', '--esmaxsize', default=ES_MAXSIZE, help='The elasticsearch max chunk size.')
+    parser.add_argument('-r', '--ethrpcurl', default=ETH_URL, help='The Ethereum RPC node url and port.')
     args = parser.parse_args()
 
-    if (args.file):
+    if args.file:
         with open(args.file) as f:
             CONTENT = f.readlines()
             block_list = [int(x) for x in CONTENT if x.strip() and len(x.strip()) <= 8]
     else:
-        block_list = list(range(int(args.start), int(args.end)))
-        
+        block_list = list(range(int(args.start_block), int(args.end_block)))
+
     ES_MAXSIZE = int(args.esmaxsize)
     ES_URL = args.esurl
+    ETH_URL = args.ethrpcurl
 
     chunks_arr = list(chunks(block_list, CHUNK_SIZE))
 
