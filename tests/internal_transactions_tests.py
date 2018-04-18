185c200227eafb2ad9b5bfa0fd8fbbeef9d934da
Your Name <you@example.com>
2018-04-18 17:06:24 +0300

    Fixed test

--- tests/internal_transactions_tests.py
+++ tests/internal_transactions_tests.py
@@ -6,6 +6,7 @@ import random
 import requests
 import json
 from multiprocessing import Pool
+from tqdm import *
 
 class SergeImplementation():
   def _http_post_request(self, url, request):
@@ -24,7 +25,7 @@ class SergeImplementation():
     pool = Pool(processes=10)
     def f(hash):
       self._http_post_request(
-      'http://localhost:8545', 
+      'http://localhost:8545',
       self._make_request_trace(hash))['result']['trace']
     return pool.map(f, hashes)
 
@@ -66,17 +67,19 @@ class InternalTransactionsTestCase(unittest.TestCase):
   def test_get_traces_faster_than_serge(self):
     real_client = ElasticSearch('http://localhost:9200')
     attemps = []
-    real_hashes = real_client.search(index=INDEX_WITH_REAL_DATA, doc_type="tx", query='to_contract:true', size=TEST_BIG_TRANSACTIONS_NUMBER*TEST_ATTEMPS)['hits']['hits']
+    real_hashes = real_client.search(index=INDEX_WITH_REAL_DATA, doc_type="tx", query='to_contract:true', size=TEST_BIG_TRANSACTIONS_NUMBER*TEST_ATTEMPS*3)['hits']['hits']
     real_hashes = [t["_source"]["hash"] for t in real_hashes]
     serge_implementation = SergeImplementation()
-    for attemp in range(TEST_ATTEMPS):
-      start_chunk = attemp * TEST_BIG_TRANSACTIONS_NUMBER
-      end_chunk = (attemp + 1) * TEST_BIG_TRANSACTIONS_NUMBER  
+    for attemp in tqdm(range(0, TEST_ATTEMPS*3, 3)):
+      start_serge_chunk = attemp * TEST_BIG_TRANSACTIONS_NUMBER
+      end_serge_chunk = (attemp + 1) * TEST_BIG_TRANSACTIONS_NUMBER
+      start_my_chunk = (attemp + 1) * TEST_BIG_TRANSACTIONS_NUMBER
+      end_my_chunk = (attemp + 2) * TEST_BIG_TRANSACTIONS_NUMBER
       start_time_for_serge_extractor = time()
-      response = serge_implementation.get_traces(real_hashes[start_chunk:end_chunk])
+      response = serge_implementation.get_traces(real_hashes[start_serge_chunk:end_serge_chunk])
       assert len(response) == TEST_BIG_TRANSACTIONS_NUMBER
       start_time_for_my_extractor = time()
-      response = self.internal_transactions._get_traces({i: hash for i, hash in enumerate(real_hashes[start_chunk:end_chunk])})
+      response = self.internal_transactions._get_traces({i: hash for i, hash in enumerate(real_hashes[start_my_chunk:end_my_chunk])})
       assert len(response.keys()) == TEST_BIG_TRANSACTIONS_NUMBER
       end_time = time()
       serge_time = start_time_for_my_extractor - start_time_for_serge_extractor
@@ -120,7 +123,7 @@ class InternalTransactionsTestCase(unittest.TestCase):
     pass
 
 TEST_TRANSACTIONS_NUMBER = 10
-TEST_BIG_TRANSACTIONS_NUMBER = TEST_TRANSACTIONS_NUMBER * 100
+TEST_BIG_TRANSACTIONS_NUMBER = TEST_TRANSACTIONS_NUMBER * 10
 TEST_INDEX = 'test-ethereum-transactions'
 TEST_TRANSACTION_HASH = '0x38a999ebba98a14a67ea7a83921e3e58d04a29fc55adfa124a985771f323052a'
 TEST_TRANSACTION_INPUT = '0xb1631db29e09ec5581a0ec398f1229abaf105d3524c49727621841af947bdc44'
