55a3ae5d55f85655c51e0d54438df82f72362c2d
Anatoli Belchikov <belchikov1996@gmail.com>
2018-05-16 08:03:43 -0400

    Added installation scripts

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt-get install libcurl4-openssl-dev cmake python3-pip libpython3-dev libssl-dev build-essential automake pkg-config libtool libffi-dev libgmp-dev libyaml-cpp-dev
+
+sudo -H pip3 install -r ./requirements.txt
+
+git clone https://github.com/ethereum/pyethereum
+cd ./pyethereum
+sudo -H pip3 install .
+cd ../
+
+git clone https://github.com/Great-Hill-Corporation/quickBlocks
+cd ./quickBlocks/src
+cmake .
+make
+cd ../../
+
+echo "ssh -L 9200:localhost:9200 cyberdrop@195.201.105.114"
+echo "nano ./config.py"
