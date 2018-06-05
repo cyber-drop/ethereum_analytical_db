a850eed173ecae37f0f110b274f70f0b74e7e338
ninjascant <cvlt.mao@gmail.com>
2018-06-05 21:37:30 +0500

    Add tranfer values rounding

--- token_holders.py
+++ token_holders.py
@@ -4,6 +4,8 @@ from config import INDICES
 import requests
 import json
 from pyelasticsearch import bulk_chunks
+import math
+from decimal import Decimal
 
 class TokenHolders:
   def __init__(self, elasticsearch_indices=INDICES, elasticsearch_host="http://localhost:9200", tx_index='internal'):
@@ -11,10 +13,11 @@ class TokenHolders:
     self.client = CustomElasticSearch(elasticsearch_host)
     self.tx_index = self.indices['internal_transaction'] if tx_index == 'internal' else self.indices['transaction']
     self.tx_type = 'itx' if tx_index == 'internal' else 'tx'
+    self.token_decimals = {}
 
   def _construct_bulk_insert_ops(self, docs):
     for doc in docs:
-      yield self.client.index_op(doc)
+      yield self.client.index_op(doc, id=doc['tx_hash'])
 
   def _insert_multiple_docs(self, docs, doc_type, index_name):
     for chunk in bulk_chunks(self._construct_bulk_insert_ops(docs), docs_per_chunk=1000):
@@ -31,15 +34,24 @@ class TokenHolders:
     }
     return self.client.iterate(self.tx_index, self.tx_type, query)
 
+  def _convert_transfer_value(self, value, decimals):
+    if decimals == 1:
+      return value
+    value = int(value)
+    rounded = value / math.pow(10, decimals)
+    rounded = "{0:.5f}".format(rounded)
+    return rounded
+
   def _construct_tx_descr_from_input(self, tx):
     tx_input = tx['decoded_input']
     is_valid_tx = 'error' not in tx.keys()
+    decimals = self.token_decimals[tx['to']] if tx['to'] in self.token_decimals.keys() else 1
     if tx_input['name'] == 'transfer':
-      return {'method': tx_input['name'], 'from': tx['from'], 'to': tx_input['params'][0]['value'], 'value': tx_input['params'][1]['value'],'block_id': tx['blockNumber'], 'valid': is_valid_tx, 'token': tx['to'], 'tx_index': self.indices['internal_transaction']}
+      return {'method': tx_input['name'], 'from': tx['from'], 'to': tx_input['params'][0]['value'], 'value': self._convert_transfer_value(tx_input['params'][1]['value'], decimals),'block_id': tx['blockNumber'], 'valid': is_valid_tx, 'token': tx['to'], 'tx_index': self.indices['internal_transaction'], 'tx_hash': tx['hash']}
     elif tx_input['name'] == 'transferFrom':
-      return {'method': tx_input['name'], 'from': tx_input['params'][0]['value'], 'to': tx_input['params'][1]['value'], 'value': tx_input['params'][2]['value'], 'block_id': tx['blockNumber'], 'valid': is_valid_tx, 'token': tx['to'], 'tx_index': self.indices['internal_transaction']}
+      return {'method': tx_input['name'], 'from': tx_input['params'][0]['value'], 'to': tx_input['params'][1]['value'], 'value': self._convert_transfer_value(tx_input['params'][2]['value'], decimals), 'block_id': tx['blockNumber'], 'valid': is_valid_tx, 'token': tx['to'], 'tx_index': self.indices['internal_transaction'], 'tx_hash': tx['hash']}
     elif tx_input['name'] == 'approve':
-      return {'method': tx_input['name'], 'from': tx['from'], 'spender': tx_input['params'][0]['value'], 'value': tx_input['params'][1]['value'],'block_id': tx['blockNumber'], 'valid': is_valid_tx, 'token': tx['to'], 'tx_index': self.indices['internal_transaction']}
+      return {'method': tx_input['name'], 'from': tx['from'], 'spender': tx_input['params'][0]['value'], 'value': self._convert_transfer_value(tx_input['params'][1]['value'], decimals),'block_id': tx['blockNumber'], 'valid': is_valid_tx, 'token': tx['to'], 'tx_index': self.indices['internal_transaction'], 'tx_hash': tx['hash']}
     else:
       return
 
@@ -66,7 +78,8 @@ class TokenHolders:
 
   def get_listed_tokens_txs(self):
     for tokens in self._iterate_tokens():
-        self._extract_tokens_txs([token['_source']['address'] for token in tokens])
+      self.token_decimals = {token['_source']['address']: token['_source']['decimals'] for token in tokens if 'decimals' in token['_source'].keys()}
+      self._extract_tokens_txs([token['_source']['address'] for token in tokens])
 
   def _get_listed_tokens_addresses(self):
     addresses = []
