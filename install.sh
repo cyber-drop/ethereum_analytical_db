19f969b3b41b5f22b7b4f5241460e89d2155bd31
Anatoli Belchikov <belchikov1996@gmail.com>
2018-06-11 19:48:20 +0300

    Freeze packages

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
+sudo -H mkdir /usr/local/share/man
+
 sudo apt-get install libcurl4-openssl-dev cmake python3-pip libpython3-dev libssl-dev build-essential automake pkg-config libtool libffi-dev libgmp-dev libyaml-cpp-dev
 
 sudo -H pip3 install -r ./requirements.txt
 
 git clone https://github.com/ethereum/pyethereum
 cd ./pyethereum
+git checkout develop
+git checkout 3d5ec14032cc471f4dcfc7cc5c947294daf85fe0
 sudo -H pip3 install .
 cd ../
 
 git clone https://github.com/Great-Hill-Corporation/quickBlocks
 cd ./quickBlocks/src
+git checkout master
+git checkout 05f305ac3ce8eea27a21b52606588527f0131640
 cmake .
 make
 sudo -H make install
