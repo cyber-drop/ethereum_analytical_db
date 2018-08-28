14b57c593afd0dbceaeecac55f9a90f6de87d6a2
ninjascant <cvlt.mao@gmail.com>
2018-08-28 17:57:48 +0500

    Fix check for decimals

--- tests/token_holders_tests.py
+++ tests/token_holders_tests.py
@@ -29,10 +29,11 @@ class ExternalTokenTransactionsTestCase(TokenHoldersTestCase, unittest.TestCase)
     token_txs = self.token_holders._iterate_token_tx_descriptions('0x5ca9a71b1d01849c0a95490cc00559717fcf0d1d')
     token_txs = [tx for txs_list in token_txs for tx in txs_list]
     methods = [tx['_source']['method'] for tx in token_txs]
-    amounts = [tx['_source']['raw_value'] for tx in token_txs]
+    amounts = [tx['_source']['value'] for tx in token_txs]
+    
     with_error = [tx for tx in token_txs if tx['_source']['valid'] == False]
-    self.assertCountEqual(['transfer', 'approve', 'transferFrom'], methods)
-    self.assertCountEqual(['356245680000000000000', '356245680000000000000', '2266000000000000000000'], amounts)
+    self.assertCountEqual(['transfer', 'transferFrom'], methods)
+    self.assertCountEqual([356.24568, 2266.0], amounts)
     assert len(with_error) == 1
 
   def test_iterate_unprocessed_tokens(self):
@@ -68,18 +69,6 @@ class ExternalTokenTransactionsTestCase(TokenHoldersTestCase, unittest.TestCase)
     tokens = [t for token in tokens for t in token]
     flags = [token['_source']['tx_descr_scanned'] for token in tokens]
     self.assertCountEqual([True, True], flags)
-  '''
-  def test_run(self):
-    for i, address in enumerate(TEST_TOKEN_ADDRESSES):
-      self.client.index(TEST_INDEX, 'contract', {'address': address, 'cmc_id': str(1234+i), 'token_name': TEST_TOKEN_NAMES[i], 'token_symbol': TEST_TOKEN_SYMBOLS[i], 'abi': ['mock_abi']}, refresh=True)
-    self.token_holders.run(TEST_BLOCK)
-    
-    all_descrptions = self.token_holders._iterate_tx_descriptions()
-    all_descrptions = [tx for txs_list in all_descrptions for tx in txs_list]
-    token = list(set([descr['_source']['token'] for descr in all_descrptions]))[0]
-    assert token == '0x5ca9a71b1d01849c0a95490cc00559717fcf0d1d'
-    assert len(all_descrptions) == 2
-  '''
 
   def test_set_transaction_index(self):
     self.client.index(TEST_INDEX, 'contract', {'address': TEST_TOKEN_ADDRESSES[0], 'cmc_id': '1234', 'token_name': TEST_TOKEN_NAMES[0], 'token_symbol': TEST_TOKEN_SYMBOLS[0], 'abi': ['mock_abi'], 'decimals': 18}, id=TEST_TOKEN_ADDRESSES[0], refresh=True)
@@ -94,8 +83,8 @@ class ExternalTokenTransactionsTestCase(TokenHoldersTestCase, unittest.TestCase)
     self.assertCountEqual([TEST_TX_INDEX], tx_indices)
 
   def test_extract_contract_creation_descr(self):
-    self.client.index(TEST_INDEX, 'contract', {'address': TEST_TOKEN_ADDRESSES[0], 'total_supply': '100000000', 'blockNumber': 5000000, 'owner': '0x1554aa0026292d03cfc8a2769df8dd4d169d590a', 'parent_transaction': TEST_PARENT_TXS[0], 'cmc_id': str(1234), 'token_name': TEST_TOKEN_NAMES[0], 'token_symbol': TEST_TOKEN_SYMBOLS[0], 'abi': ['mock_abi']}, id=TEST_TOKEN_ADDRESSES[0], refresh=True)
-    self.client.index(TEST_INDEX, 'contract', {'address': TEST_TOKEN_ADDRESSES[1], 'total_supply': '200000000', 'blockNumber': 5000010, 'token_owner': '0x17Bc58b788808DaB201a9A90817fF3C168BF3d61', 'parent_transaction': TEST_PARENT_TXS[1], 'cmc_id': str(1235), 'token_name': TEST_TOKEN_NAMES[0], 'token_symbol': TEST_TOKEN_SYMBOLS[1], 'abi': ['mock_abi']}, id=TEST_TOKEN_ADDRESSES[1], refresh=True)
+    self.client.index(TEST_INDEX, 'contract', {'address': TEST_TOKEN_ADDRESSES[0], 'total_supply': '100000000', 'blockNumber': 5000000, 'owner': '0x1554aa0026292d03cfc8a2769df8dd4d169d590a', 'parent_transaction': TEST_PARENT_TXS[0], 'cmc_id': str(1234), 'token_name': TEST_TOKEN_NAMES[0], 'token_symbol': TEST_TOKEN_SYMBOLS[0], 'abi': ['mock_abi'], 'decimals': 18}, id=TEST_TOKEN_ADDRESSES[0], refresh=True)
+    self.client.index(TEST_INDEX, 'contract', {'address': TEST_TOKEN_ADDRESSES[1], 'total_supply': '200000000', 'blockNumber': 5000010, 'token_owner': '0x17Bc58b788808DaB201a9A90817fF3C168BF3d61', 'parent_transaction': TEST_PARENT_TXS[1], 'cmc_id': str(1235), 'token_name': TEST_TOKEN_NAMES[0], 'token_symbol': TEST_TOKEN_SYMBOLS[1], 'abi': ['mock_abi'], 'decimals': 18}, id=TEST_TOKEN_ADDRESSES[1], refresh=True)
     for tx in TEST_TOKEN_TXS:
       self.client.index(TEST_TX_INDEX, 'tx', tx, refresh=True)
     self.token_holders.get_listed_tokens_txs()
@@ -172,7 +161,6 @@ TEST_TOKEN_NAMES = ['Aeternity', 'Golem Network Token']
 TEST_TOKEN_SYMBOLS = ['AE', 'GNT']
 TEST_TOKEN_TXS = [
   {'from': '0x6b25d0670a34c1c7b867cd9c6ad405aa1759bda0', 'to': '0x5ca9a71b1d01849c0a95490cc00559717fcf0d1d', 'decoded_input': {'name': 'transfer', 'params': [{'type': 'address', 'value': '0xa60c4c379246a7f1438bd76a92034b6c82a183a5'}, {'type': 'uint256', 'value': '2266000000000000000000'}]}, 'blockNumber': 5635149, 'hash': '0xd8f583bcb81d12dc2d3f18e0a015ef0f6e71c177913ef8f251e37b6e4f7f1f26'},
-  {'from': '0x58d46475da68984bacf1f2843b85e0fdbcbc6cef', 'to': '0x5ca9a71b1d01849c0a95490cc00559717fcf0d1d', 'decoded_input': {'name': 'approve', 'params': [{'type': 'address', 'value': '0x4e6b129bbb683952ed1ec935c778d74a77b352ce'}, {'type': 'uint256', 'value': '356245680000000000000'}]}, 'blockNumber': 5635141, 'hash': '0x4fc7d7027751eb605df79c63265ab83408d98179d7c8299c74a8336e5c3811ca'},
   {'from': '0xc917e19946d64aa31d1aeacb516bae2579995aa9', 'to': '0x5ca9a71b1d01849c0a95490cc00559717fcf0d1d', 'error': 'Out of gas', 'decoded_input': {'name': 'transferFrom', 'params': [{'type': 'address', 'value': '0xc917e19946d64aa31d1aeacb516bae2579995aa9'}, {'type': 'address', 'value': '0x4e6b129bbb683952ed1ec935c778d74a77b352ce'}, {'type': 'uint256', 'value': '356245680000000000000'}]}, 'blockNumber': 5635142, 'hash': '0xca811570188b2e5d186da8292eda7e0bf7dde797a68d90b9ac2e014e321a94b2'},
   {'from': '0x6b25d0670a34c1c7b867cd9c6ad405aa1759bda0', 'to': '0x5ca9a71b1d01849c0a95490cc00559717fcf0d1d', 'blockNumber': 5635149, 'hash': '0x2497b3dcbce36c4d2cbe42931fa160cb39703ae5487bf73044520410101e7c8c'},
   {'from': '0x892ce7dbc4a0efbbd5933820e53d2c945ef9f722', 'to': '0x51ada638582e51c931147c9abd2a6d63bc02e337', 'decoded_input': {'name': 'transfer', 'params': [{'type': 'address', 'value': '0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be'}, {'type': 'uint256', 'value': '2294245680000000000000'}]}, 'blockNumber': 5632141, 'hash': '0x4188f8c914b5f58f911674ff766d45da2a19c1375a8487841dc4bdb5214c3aa2'},
