bec5bd2d448890803a66c69f446c065309931024
Anatoli Belchikov <belchikov1996@gmail.com>
2018-09-06 13:15:35 +0300

    Added documentation for prepare-blocks

--- blocks.py
+++ blocks.py
@@ -21,6 +21,15 @@ class Blocks:
     self.w3 = Web3(HTTPProvider('http://localhost:8550'))
 
   def _get_max_parity_block(self):
+    """
+    Get last block in parity
+
+    Returns
+    -------
+    int:
+        Last block number
+        0 if there are no blocks in parity
+    """
     response = requests.post(self.parity_host, data=json.dumps({
       "id": 1,
       "jsonrpc": "2.0",
@@ -32,6 +41,15 @@ class Blocks:
     return int(response["result"], 0)
 
   def _get_max_elasticsearch_block(self):
+    """
+    Get last block in ElasticSearch
+
+    Returns
+    -------
+    int:
+        Last block number
+        0 if there are no blocks in ElasticSearch
+    """
     aggregation = {
       "size": 0,
       "aggs": {
@@ -50,12 +68,36 @@ class Blocks:
       return 0
 
   def _extract_block_timestamp(self, block_number):
+    """
+    Get block timestamp from parity
+
+    Parameters
+    ----------
+    block_number : int
+        Block number
+
+    Returns
+    -------
+    datetime
+        Timestamp of a block
+        None if no such block in parity
+    """
     block = self.w3.eth.getBlock(block_number)
     if block != None:
       timestamp = block.timestamp
       return datetime.datetime.fromtimestamp(timestamp)
 
   def _create_blocks(self, start, end):
+    """
+    Create blocks from start to end. Extract timestamps for each block
+
+    Parameters
+    ----------
+    start : int
+        Start block number
+    end : int
+        End block number
+    """
     docs = [{
       "number": i,
       'id': i,
@@ -67,6 +109,11 @@ class Blocks:
         self.client.bulk_index(docs=chunk, index=self.indices["block"], doc_type="b", refresh=True)
 
   def create_blocks(self):
+    """
+    Create blocks from last block in ElasticSearch to last parity block in ElasticSearch
+
+    This function is an entry point for prepare-blocks operation
+    """
     max_parity_block = self._get_max_parity_block()
     max_elasticsearch_block = self._get_max_elasticsearch_block()
     self._create_blocks(max_elasticsearch_block + 1, max_parity_block)
