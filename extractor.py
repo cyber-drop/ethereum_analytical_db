9c1bc4551a8e0083befe4d964a191f69523f8bbb
Anatoli Belchikov <belchikov1996@gmail.com>
2019-02-12 18:18:21 +0300

    Removed unused operations

--- extractor.py
+++ extractor.py
@@ -4,21 +4,16 @@ from operations import clickhouse
 from config import DATABASE
 
 OPERATIONS = {
-  "elasticsearch": {
-    "prepare-indices": elasticsearch.prepare_indices
-  },
   "clickhouse": {
     "prepare-indices": clickhouse.prepare_indices,
     "prepare-blocks": clickhouse.prepare_blocks,
     "prepare-contracts-view": clickhouse.prepare_contracts_view,
     "extract-traces": clickhouse.extract_traces,
     "extract-contracts-abi": clickhouse.extract_contracts_abi,
-    "extract-transaction-fees": clickhouse.extract_transaction_fees,
     "extract-events": clickhouse.extract_events,
     "parse-transactions-inputs": clickhouse.parse_transactions_inputs,
     "parse-events-inputs": clickhouse.parse_events_inputs,
     "extract-token-transactions": clickhouse.extract_token_transactions,
-    "extract-multitransfers": clickhouse.extract_multitransfers,
     "extract-prices": clickhouse.extract_prices
   }
 }
