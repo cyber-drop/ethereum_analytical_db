2c25a8a21f4dfaf9a352efcf61cb0ed234b33732
Anatoli Belchikov <belchikov1996@gmail.com>
2019-02-15 18:00:29 +0300

    Rejected token prices

--- tests/token_prices_tests.py
+++ tests/token_prices_tests.py
@@ -1,136 +1,144 @@
 import unittest
-from token_prices import TokenPrices
-from tests.test_utils import TestElasticSearch
+from operations.token_prices import ClickhouseTokenPrices
 from unittest import mock
-import datetime
-import codecs
-import json
-import os
+from tests.test_utils import TestClickhouse
+from datetime import datetime, timedelta
+import config
+from tests.test_utils import parity
 
-today = datetime.date.today().strftime('%Y%m%d')
 
-def mocked_requests_get(*args, **kwargs):
-  class MockResponse:
-    def __init__(self, json_data, status_code, text=None):
-      self.json_data = json_data
-      self.status_code = status_code
-      self.text = text
-    def json(self):
-      return self.json_data
-  tests_dir = os.path.dirname(__file__)
-  if args[0] == 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD':
-    return MockResponse({"BTC":{"USD":6508.66},"ETH":{"USD":470.14}}, 200)
-  elif args[0] == 'https://coinmarketcap.com/currencies/binance-coin/historical-data/?start=20130428&end={}'.format(today):
-    test_token_page = open('{}/cmc_token_page.html'.format(tests_dir), "r").read() 
-    return MockResponse({}, 200, test_token_page)
-  elif args[0] == 'https://coinmarketcap.com/tokens/views/all/':
-    test_token_list_page = open('{}/cmc_token_list_page.html'.format(tests_dir), "r").read()
-    return MockResponse({}, 200, test_token_list_page)
-  elif args[0] == 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=3000':
-    with open('{}/test_cmc_api_res.json'.format(tests_dir)) as json_file:
-      test_cmc_api_res = json.load(json_file)
-    return MockResponse(test_cmc_api_res, 200)
-  else:
-    return MockResponse(TEST_RES, 200)
-  return MockResponse(None, 404)
+class ClickhouseTokenPricesTestCase(unittest.TestCase):
+    def setUp(self):
+        config.PROCESSED_CONTRACTS.clear()
+        self.client = TestClickhouse()
+        self.indices = {
+            'contract': TEST_CONTRACT_INDEX,
+            'contract_block': TEST_CONTRACT_BLOCK_INDEX,
+            'price': TEST_PRICES_INDEX
+        }
+        self.client.prepare_indices(self.indices)
+        self.token_prices = ClickhouseTokenPrices(self.indices, TEST_PARITY_URL)
 
-class TokenPricesTestCase(unittest.TestCase):
-  def setUp(self):
-    self.client = TestElasticSearch()
-    self.token_prices = TokenPrices({'contract': TEST_CONTRACT_INDEX, 'token_price': TEST_PRICES_INDEX})
-    self.client.recreate_index(TEST_PRICES_INDEX)
-    self.client.recreate_index(TEST_CONTRACT_INDEX)
-  def _iterate_prices(self):
-    return self.token_prices.client.iterate(TEST_PRICES_INDEX, 'price', 'token:*')
+    def test_iterate_tokens_from_whitelist(self):
+        test_contracts = [{
+            "id": 1,
+            "address": "0x1",
+            "standard_erc20": 1
+        }, {
+            "id": 2,
+            "address": "0x2",
+            "standard_erc20": 1
+        }]
+        self.client.bulk_index(index=TEST_CONTRACT_INDEX, docs=test_contracts)
+        config.PROCESSED_CONTRACTS.append("0x1")
 
-  @mock.patch('requests.get', side_effect=mocked_requests_get)
-  def test_get_token_list(self, mock_get):
-    tokens = self.token_prices.get_token_list()
-    assert tokens.shape == (1059, 3)
+        contracts = self.token_prices._get_cc_tokens()
 
-  @mock.patch('requests.get', side_effect=mocked_requests_get)
-  def test_get_token_cmc_historical_info(self, mock_get):
-    token_info = self.token_prices._get_token_cmc_historical_info('https://coinmarketcap.com/currencies/binance-coin/')
-    assert len(token_info) == 496
-    assert token_info[0] == {'market_cap': 686750930, 'timestamp': '2018-12-02', 'USD': 5.26, 'token': 'BNB', 'source': 'coinmarketcap'}
+        self.assertCountEqual([contract["address"] for contract in contracts], ["0x1"])
 
-  @mock.patch('requests.get', side_effect=mocked_requests_get)
-  def test_get_current_cmc_prices(self, mock_get):
-    prices = self.token_prices.get_current_cmc_prices()
-    bnb_price = {
-      'token': 'BNB', 
-      'USD': 4.62077743608, 
-      'ETH': 0.0522571339, 
-      'BTC': 0.0013527843, 
-      'source': 'coinmarketcap', 
-      'market_cap': 604394523, 
-      'timestamp': datetime.date(2018, 12, 11)
-    }
-    assert prices[0] == bnb_price
+    def test_moving_average(self):
+        test_prices = [
+            {"close": 2},
+            {"close": 3},
+            {"close": 4},
+            {"close": 5},
+            {"close": 4},
+            {"close": 200},
+            {"close": 5},
+        ]
 
-  @mock.patch('requests.get', side_effect=mocked_requests_get)
-  def test_get_recent_token_prices(self, mock_get):
-    for sym in TEST_TOKEN_SYMBOLS:
-      self.client.index(TEST_CONTRACT_INDEX, 'contract', {'cc_sym': sym}, refresh=True)
-    self.token_prices.get_recent_token_prices()
-    prices = self._iterate_prices()
-    prices = [p['_source'] for price in prices for p in price]
-    prices_fsyms = [p['token'] for p in prices]
-    prices_usd = [p['USD'] for p in prices]
-    self.assertCountEqual([0.1030971744, 0.0412649044, 2.84103009], prices_usd)
+        self.token_prices._set_moving_average(test_prices, window_size=5)
 
-  def test_moving_average(self):
-    test_prices = [
-      {"close": 2},
-      {"close": 3},
-      {"close": 4},
-      {"close": 5},
-      {"close": 4},
-      {"close": 200},
-      {"close": 5},
-    ]
+        self.assertSequenceEqual([price["average"] for price in test_prices], [
+            2,
+            3,
+            4,
+            5,
+            (2 + 3 + 4 + 5 + 4) / 5,
+            (3 + 4 + 5 + 4 + 200) / 5,
+            (4 + 5 + 4 + 200 + 5) / 5,
+        ])
 
-    self.token_prices._set_moving_average(test_prices)
+    def test_moving_average_usage(self):
+        test_prices = [{
+            "open": 1,
+            "close": 2,
+            "average": 10,
+            'time': 1415463675,
+            'address': "0x1"
+        }]
+        self.token_prices.btc_prices = {
+            '2014-11-08': 0
+        }
+        self.token_prices.eth_prices = {
+            '2014-11-08': 0
+        }
+        self.token_prices._set_moving_average = mock.MagicMock()
 
-    self.assertSequenceEqual([price["average"] for price in test_prices], [
-      2,
-      3,
-      4,
-      5,
-      (2 + 3 + 4 + 5 + 4) / 5,
-      (3 + 4 + 5 + 4 + 200) / 5,
-      (4 + 5 + 4 + 200 + 5) / 5,
-    ])
+        result = self.token_prices._process_hist_prices(test_prices)
 
-  def test_moving_average_usage(self):
-    test_prices = [{
-      "open": 1,
-      "close": 2,
-      "average": 10,
-      'time': 1415463675,
-      'token': "JAT"
-    }]
-    self.token_prices.btc_prices = {
-      '2014-11-08': 0
-    }
-    self.token_prices.eth_prices = {
-      '2014-11-08': 0
-    }
-    self.token_prices._set_moving_average = mock.MagicMock()
-    self.token_prices._to_usd = mock.MagicMock(return_value=0)
-    self.token_prices._from_usd = mock.MagicMock(return_value=0)
+        self.token_prices._set_moving_average.assert_any_call(test_prices)
+        self.assertSequenceEqual([price["BTC"] for price in result], [10])
 
-    result = self.token_prices._process_hist_prices(test_prices)
+    def test_get_last_avail_price_date(self):
+        test_current_datetime = datetime.today()
+        test_prices = [{
+            "id": 1,
+            "timestamp": test_current_datetime - timedelta(days=10)
+        }, {
+            "id": 2,
+            "timestamp": test_current_datetime
+        }]
 
-    self.token_prices._set_moving_average.assert_any_call(test_prices)
-    self.assertSequenceEqual([price["BTC"] for price in result], [10])
+        self.client.bulk_index(index=TEST_PRICES_INDEX, docs=test_prices)
+        last_date = self.token_prices._get_last_avail_price_date()
+        assert (last_date - test_current_datetime).days < 1
 
-  def test_get_last_day_empty_index(self):
-    last_date = self.token_prices._get_last_avail_price_date()
-    self.assertSequenceEqual(last_date, ["2013", "01", "01"])
+    def test_get_last_day_empty_index(self):
+        last_date = self.token_prices._get_last_avail_price_date()
+        assert (last_date - datetime(1970, 1, 1)).days < 1
 
-TEST_PRICES_INDEX = 'test-token-prices'
-TEST_CONTRACT_INDEX = 'test-ethereum-contract'
+    @parity
+    def test_get_symbol_by_address(self):
+        test_contracts = {
+            "0xf230b790e05390fc8295f4d3f60332c93bed42e2": "TRX",
+            "0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0": "EOS",
+            "0xb5a5f22694352c15b00323844ad545abb2b11028": ""
+        }
+        for address, symbol in test_contracts.items():
+            result = self.token_prices._get_symbol_by_address(address)
+            assert symbol == result
+
+    def test_get_days_count(self):
+        test_dates = [
+            datetime.now() - timedelta(days=10),
+            datetime.now() - timedelta(days=3000)
+        ]
+        test_limit = 2000
+        test_days = [11, test_limit]
+        test_last_date = datetime.now()
+        for i, date in enumerate(test_dates):
+            result = self.token_prices._get_days_count(test_last_date, date, limit=test_limit)
+            assert test_days[i] == result
+
+    @parity
+    def test_process(self):
+        test_contracts = ["0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0"]
+        self.client.bulk_index(
+            index=self.indices["contract"],
+            docs=[{"address": contract, "id": contract, "standard_erc20": True} for contract in test_contracts]
+        )
+        self.token_prices.get_prices_within_interval()
+        prices = self.client.search(index=TEST_PRICES_INDEX, fields=["BTC", "address", "timestamp"])
+        print(prices)
+        assert len(prices)
+
+
+TEST_PARITY_URL = "http://localhost:8545"
+TEST_PRICES_INDEX = 'test_token_prices'
+TEST_CONTRACT_INDEX = 'test_ethereum_contract'
+TEST_CONTRACT_BLOCK_INDEX = 'test_ethereum_contract_block'
 TEST_TOKEN_SYMBOLS = ['AE', 'FND', 'CPAY', 'SEXC']
-TEST_ADDRESSES = ['0x5ca9a71b1d01849c0a95490cc00559717fcf0d1d', '0x4df47b4969b2911c966506e3592c41389493953b', '0x0ebb614204e47c09b6c3feb9aaecad8ee060e23e', '0x2567c677473d110d75a8360c35309e63b1d52429']
-TEST_RES = {"AE":{"BTC":0.0004365},"FND":{"BTC":0.00001584},"CPAY":{"BTC":0.00000634}}
+TEST_ADDRESSES = ['0x5ca9a71b1d01849c0a95490cc00559717fcf0d1d', '0x4df47b4969b2911c966506e3592c41389493953b',
+                  '0x0ebb614204e47c09b6c3feb9aaecad8ee060e23e', '0x2567c677473d110d75a8360c35309e63b1d52429']
+TEST_RES = {"AE": {"BTC": 0.0004365}, "FND": {"BTC": 0.00001584}, "CPAY": {"BTC": 0.00000634}}
