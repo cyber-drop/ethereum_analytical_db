0a7cbf38dedbdee64ac8d7c4601e5bf1bc575542
Anatoli Belchikov <belchikov1996@gmail.com>
2019-03-06 17:37:32 +0300

    Added tabix container with custom dashboards

---
+++ tabix/dashboards.js
@@ -0,0 +1,24 @@
+var LS_SESSION_DATA = "ls.sessionData"
+var DASHBOARDS = [{
+    "name":"eth SQL",
+    "sql":"Zdes budet neskolko sluchainyh slov",
+    "buttonTitle": "Run all ⇧ + ⌘ + ⏎",
+    "format":{},
+    "results":[],
+    "editor":null,
+    "selectedResultTab":0
+}]
+
+console.log("Injecting pre-saved dashboards...")
+
+setTimeout(function() {
+    console.log("Loading pre-saved dashboards...")
+    var sessionData = JSON.parse(localStorage.getItem(LS_SESSION_DATA) || "[]")
+    var presented = sessionData.filter(x => x.name.indexOf("eth") != -1).length
+    if (presented < DASHBOARDS.length) {
+        console.log("Dashboards added")
+        sessionData = sessionData.concat(DASHBOARDS)
+        localStorage.setItem(LS_SESSION_DATA, JSON.stringify(sessionData))
+        window.location.reload(false)
+    }
+}, 3000)
