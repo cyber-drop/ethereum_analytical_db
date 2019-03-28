4f6838ff78496f386cc6f716328e739a4ea4a6d8
Anatoli Belchikov <belchikov1996@gmail.com>
2019-03-29 00:49:37 +0300

    Added max size of chunk

--- tests/clickhouse_tests.py
+++ tests/clickhouse_tests.py
@@ -2,6 +2,8 @@ import unittest
 from clickhouse_driver import Client
 from clients.custom_clickhouse import CustomClickhouse
 import json
+import sys
+from unittest.mock import MagicMock, ANY, call
 
 
 class ClickhouseTestCase(unittest.TestCase):
@@ -94,7 +96,41 @@ class ClickhouseTestCase(unittest.TestCase):
         result = self.client.execute('SELECT dict FROM test')
         self.assertCountEqual(result, [(json.dumps(doc["dict"]),) for doc in documents])
 
+    def test_bulk_index_split_records(self):
+        test_docs = [{"docs": True}]
+        test_chunks = ["records1", "records2"]
+        self.new_client._split_records = MagicMock(return_value=test_chunks)
+        self.new_client.client.execute = MagicMock()
+        self.new_client._set_id = MagicMock()
+        self.new_client._filter_schema = MagicMock()
+        self.new_client.bulk_index(index="test_index", docs=test_docs)
+
+        self.new_client._split_records.assert_called_with(test_docs)
+        calls = [call(ANY, records) for records in test_chunks]
+        self.new_client.client.execute.assert_has_calls(calls)
+
     def test_send_sql_request(self):
         formatted_documents = self._add_records()
         result = self.new_client.send_sql_request("SELECT max(x) FROM test")
         assert result == max(doc["_source"]["x"] for doc in formatted_documents)
+
+    def test_split_records(self):
+        test_record = {"test": "123"}
+        test_record_size = sys.getsizeof(test_record)
+        test_records = [test_record] * 5
+        chunks = list(self.new_client._split_records(test_records, max_bytes=test_record_size * 2 + 1))
+        self.assertSequenceEqual(chunks, [[test_record] * 2, [test_record] * 2, [test_record]])
+
+    def test_split_records_one_record(self):
+        test_record = {"test": "123"}
+        test_record_size = sys.getsizeof(test_record)
+        test_records = [test_record]
+        chunks = list(self.new_client._split_records(test_records, max_bytes=test_record_size))
+        self.assertSequenceEqual(chunks, [[test_record]])
+
+    def test_split_records_same_chunk(self):
+        test_record = {"test": "123"}
+        test_record_size = sys.getsizeof(test_record)
+        test_records = [test_record] * 6
+        chunks = list(self.new_client._split_records(test_records, max_bytes=test_record_size * 2))
+        self.assertSequenceEqual(chunks, [[test_record] * 2, [test_record] * 2, [test_record] * 2])
